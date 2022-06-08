f5b59c6483f47a14a401432def49abb00de62adb
Nivedita Mandal <51962570+NiveditaMandal@users.noreply.github.com>
2022-06-09 03:12:39 +0530

    Create Q26_Addition_Of_Two_Numbers.java

---
+++ Q26_Addition_Of_Two_Numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Q26_Addition_Of_Two_Numbers{
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        System.out.println("Enter the 1st number ");
+        int n1= in.nextInt();
+        System.out.println("Enter the 2nd number");
+        int n2= in.nextInt();
+        System.out.println("Addition of two number is "+ (n1+n2));
+    }
+}
