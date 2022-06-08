6bec8ce71b2586ef9ed49e3ea23ba637f15e6eb1
Nivedita Mandal <51962570+NiveditaMandal@users.noreply.github.com>
2022-06-09 02:01:02 +0530

    Create Q18_Volume_Of_Pyramid.java

---
+++ Q18_Volume_Of_Pyramid.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Q18_Volume_Of_Pyramid{
+    public static void main(String[] args){
+        // volume of pyramid
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the height of the pyramid: ");
+        double height = sc.nextInt();
+        System.out.println("Enter the width of the pyramid: ");
+        double width = sc.nextInt();
+        System.out.println("Enter the length of the pyramid: ");
+        double length = sc.nextInt();
+        double volume = (height * width * length) / 3;
+        System.out.println("The volume of the pyramid is: " + volume);
+        sc.close();
+    }
+}
